d166459c51e0f9c90d288211def75e96b062dcc7
Dominique VASSARD <dominique.vassard@gmail.com>
2017-04-17 12:51:27 +0200

    Add specific pg users

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "programming_phoenix",
+  password: "FotOdOps",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
