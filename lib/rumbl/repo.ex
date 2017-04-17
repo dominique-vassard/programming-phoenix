bc21e009a2b10e2f6fcb399fe782b684558caba3
Dominique VASSARD <dominique.vassard@gmail.com>
2017-04-17 12:51:57 +0200

    Part 1 Chapter 4: Ecto and Changesets

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,34 +1,3 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
-  @moduledoc """
-    In memory repository.
-  """
-
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1",
-                 name: "John",
-                 username: "johnduff",
-                 password: "jd456"},
-     %Rumbl.User{id: "2",
-                 name: "Jane",
-                 username: "janedoe",
-                 password: "thisone"},
-     %Rumbl.User{id: "3",
-                 name: "Jack",
-                 username: "jackdaniels",
-                 password: "3rdj"}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,3 +1,23 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use Rumbl.Web, :model
+
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @required_fields ~w(name username)
+  @optional_fields ~w(password)
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(Enum.map @required_fields, &String.to_atom/1)
+    |> validate_length(:username, min: 5, max: 20)
+  end
 end
